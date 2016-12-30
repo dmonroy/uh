875f1965d4b47d07ece230b2d8970cac82ff7eee
Darwin Monroy <contact@darwinmonroy.com>
2016-12-30 11:08:11 -0600

    creates upload directory if it doesn´t exists

--- uh/fs.py
+++ uh/fs.py
@@ -12,7 +12,9 @@ class Uploader(BaseUploader):
 
     async def file_ready(self):
         filename = await self.get_filename()
-        filepath = os.path.join(UPLOAD_PATH, filename)
+        path = await self.get_upload_path()
+        os.makedirs(path, exist_ok=True)
+        filepath = os.path.join(path, filename)
 
         with open(filepath, 'wb') as f:
             while True:
@@ -21,6 +23,9 @@ class Uploader(BaseUploader):
                     break
 
                 f.write(chunk)
+
+    async def get_upload_path(self):
+        return UPLOAD_PATH
 
     async def serve(self, filename):
         filepath = os.path.join(UPLOAD_PATH, filename)
